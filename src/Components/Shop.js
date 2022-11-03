8a99fbb5ea838c3592b7627d7c9722b35b20dc60
shailesh511 <shaileshtripathi511@gmail.com>
2022-11-04 01:33:53 +0530

    implemented by useDispatch

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,12 +1,19 @@
 import React from "react";
-
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+// import { actionCreaters } from "../State/index"
+import { withdraw } from "../State/Action-Creators";
+import { deposit } from "../State/Action-Creators";
 const Shop = () => {
+  const dispatch =  useDispatch();
     return (
         <div>
         <h2>Deposit / Withdraw money</h2>
-        <button className="btn btn-primary mx-2">-</button>
+        {/* <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreaters.withdraw(100))}}>-</button> */}
+        <button className="btn btn-primary mx-2" onClick={()=>{dispatch(withdraw(100))}}>-</button>
         Add this to cart
-        <button className="btn btn-primary mx-2">+</button>
+        {/* <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreaters.deposit(100))}}>-</button> */}
+        <button className="btn btn-primary mx-2" onClick={()=>{dispatch(deposit(100))}}>+</button>
 
         </div>
     )
